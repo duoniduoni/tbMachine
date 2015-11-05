f3534a6575449e0d915ada26f46a5cf88608d133
zhangsong-T410 <duoniduoni@126.com>
2015-11-05 17:41:16 +0800

    开始实现逻辑： 1. 检测adb是否存在。 2. 检测链接到的手机。

--- main.cpp
+++ main.cpp
@@ -1,29 +1,45 @@
 #include <stdio.h>
 #include <iostream>
+#include <stdlib.h>
 
 #include "run_cmd.h"
+
+#define END (const char *)0
 
 int main(int argc, char * argv[])
 {
 	string output = run_cmd("/usr/bin/which", 
 	"which", 
 	"ls", 
-	(const char *)0);
+	END);
 
 	printf("%s\n", output.size() ? output.c_str() : "no result!");
 
+	/*
+		检测adb是否存在 ！
+	*/
+	string pathOfAdb;
 	output = run_cmd("/usr/bin/which", 
 	"which", 
 	"adb", 
-	(const char *)0);
+	END);
 
-	printf("%s\n", output.size() ? output.c_str() : "no result!");
+	if(output.size() == 0)
+	{
+		printf("can not find adb ! exit! \n");
+		exit(0);
+	}
 
-	output = run_cmd("/bin/ls", 
-	"ls", 
-	"-a", 
-	(const char *)0);
+	pathOfAdb = output.substr(0, output.find("adb") + 3);
+	printf("\nfind adb path : %s\n", pathOfAdb.c_str());
 
+	/*
+		检测连接到的手机
+	*/
+	output = run_cmd(pathOfAdb.c_str(),
+	"adb",
+	"devices",
+	END);
 	printf("%s\n", output.size() ? output.c_str() : "no result!");
 
 	printf("<<<<<<<<<<<<<<<<<<<<<<<< main out !\n");
